a3112aeceadbcfd6ebddc6588bf6705d689949ae
singhabhudaya <95404646+singhabhudaya@users.noreply.github.com>
2024-08-22 11:26:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,27 +3,32 @@ const products = [
         name: 'Data Cable Set Kit',
         description: 'A versatile data cable set kit that includes multiple connectors for all your devices. Perfect for travel and everyday use.',
         imageUrl: 'https://i.imgur.com/2iF8uYU.jpeg',
+        price: '₹ 249'
     },
     {
         name: 'Universal Adapter with built-in Powerbank',
         description: 'Charge your devices with walk outlet or on the move.',
         imageUrl: 'https://i.imgur.com/wSz3TDE.jpeg',
+        price: '₹ 1249'
     },
     {
         name: 'Wireless Gamepad X3',
         description: 'Game on!, with X3 gamepad with phone holder.',
         imageUrl: 'https://i.imgur.com/XQvwSz9.png',
+        price: '₹ 649'
     },
     {
-        name: 'Portable Projector powerbank powered ',
-        description: 'Binge watch your favourite shows anywhere',
+        name: 'Portable Projector powerbank powered',
+        description: 'Binge watch your favourite shows anywhere.',
         imageUrl: 'https://i.imgur.com/edLTZB0.jpeg',
+        price: '₹ 1499'
     },
     {
-        name: 'Wireless Magsafe see through Powerbank',
-        description: 'Magsafe powerbank with inbuilt all type cables',
+        name: 'Wireless Magsafe See Through Powerbank',
+        description: 'Magsafe powerbank with inbuilt all type cables.',
         imageUrl: 'https://i.imgur.com/0NiuS3q.jpeg',
-    },
+        price: '₹ 1249'
+    }
 ];
 
 const productList = document.getElementById('productList');
@@ -36,6 +41,7 @@ products.forEach(product => {
         <img src="${product.imageUrl}" alt="${product.name}">
         <h2>${product.name}</h2>
         <p>${product.description}</p>
+        <p class="price">${product.price}</p>
         <button class="buy-now">Buy Now</button>
     `;
 
