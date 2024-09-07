821dd33d28330b6ff14633914c329c31be645e5d
singhabhudaya <95404646+singhabhudaya@users.noreply.github.com>
2024-09-07 22:24:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,30 +9,24 @@ const products = [
     {
         name: 'Mini Portable Printer',
         description: 'A compact printer for sticky notes, diagrams, and stickers. Print your ideas instantly!',
-        imageUrl: '',  // Add your image URL here
+        imageUrl: 'https://i.imgur.com/tP3r79p.jpeg',  // Add your image URL here
         price: '₹ 799',
         buyUrl: 'https://wa.me/+918400279228?text=I%20would%20like%20to%20buy%20your%20mini%20portable%20printer!'
     },
     {
         name: 'Rechargeable Type-C Mini Packet Sealer',
         description: 'Seal opened food packets with ease. Compact and portable with rechargeable Type-C port.',
-        imageUrl: '',  // Add your image URL here
+        imageUrl: 'https://i.imgur.com/2AlJdot.jpeg',  // Add your image URL here
         price: '₹ 549',
         buyUrl: 'https://wa.me/+918400279228?text=I%20would%20like%20to%20buy%20your%20rechargeable%20type-c%20mini%20packet%20sealer!'
     },
     {
-        name: 'Universal Adapter with built-in Powerbank',
-        description: 'Charge your devices with any outlet or on the move.',
-        imageUrl: 'https://i.imgur.com/wSz3TDE.jpeg',
+        name: 'Wireless Magsafe See Through Powerbank',
+        description: 'Magsafe powerbank with inbuilt all-type cables.',
+        imageUrl: 'https://i.imgur.com/0NiuS3q.jpeg',
         price: '₹ 1249',
-        buyUrl: 'https://wa.me/+918400279228?text=I%20would%20like%20to%20buy%20your%20universal%20adapter%20with%20powerbank!'
-    },
-    {
-        name: 'Wireless Gamepad X3',
-        description: 'Game on with the X3 gamepad with phone holder.',
-        imageUrl: 'https://i.imgur.com/XQvwSz9.png',
-        price: '₹ 649',
-        buyUrl: 'https://wa.me/+918400279228?text=I%20would%20like%20to%20buy%20your%20wireless%20gamepad%20with%20phone%20holder!'
+        buyUrl: 'https://wa.me/+918400279228?text=I%20would%20like%20to%20buy%20your%20see%20through%20magsafe%20powerbank!'        
+
     },
     {
         name: 'Portable Projector powerbank powered',
@@ -42,11 +36,18 @@ const products = [
         buyUrl: 'https://wa.me/+918400279228?text=I%20would%20like%20to%20buy%20your%20portable%20projector!'
     },
     {
-        name: 'Wireless Magsafe See Through Powerbank',
-        description: 'Magsafe powerbank with inbuilt all-type cables.',
-        imageUrl: 'https://i.imgur.com/0NiuS3q.jpeg',
+        name: 'Wireless Gamepad X3',
+        description: 'Game on with the X3 gamepad with phone holder.',
+        imageUrl: 'https://i.imgur.com/XQvwSz9.png',
+        price: '₹ 649',
+        buyUrl: 'https://wa.me/+918400279228?text=I%20would%20like%20to%20buy%20your%20wireless%20gamepad%20with%20phone%20holder!'
+    },
+    {
+        name: 'Universal Adapter with built-in Powerbank',
+        description: 'Charge your devices with any outlet or on the move.',
+        imageUrl: 'https://i.imgur.com/wSz3TDE.jpeg',
         price: '₹ 1249',
-        buyUrl: 'https://wa.me/+918400279228?text=I%20would%20like%20to%20buy%20your%20see%20through%20magsafe%20powerbank!'
+        buyUrl: 'https://wa.me/+918400279228?text=I%20would%20like%20to%20buy%20your%20universal%20adapter%20with%20powerbank!'
     }
 ];
 
